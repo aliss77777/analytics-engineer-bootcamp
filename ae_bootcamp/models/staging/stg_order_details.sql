63e6dda1b80d4b629b086391742d0308a0aae647
Alexander Liss <aliss77777@gmail.com>
2022-05-28 00:13:23 -0600

    adding all source tables

---
+++ ae_bootcamp/models/staging/stg_order_details.sql
@@ -0,0 +1,33 @@
+with source as (
+
+    select * from {{ source('northwind', 'order_details')}}
+)
+
+select * from source
+
+
+
+
+--- original query pre-macro
+/* 
+select
+company,
+last_name,
+first_name,
+email_address,
+job_title,
+business_phone,
+home_phone,
+mobile_phone,
+fax_number,
+address,
+city,
+state_province,
+zip_postal_code,
+country_region,
+web_page,
+notes,
+attachments,
+insertion_timestamp
+from 'ae-bootcamp69.dl_northwind.customer' 
+*/
